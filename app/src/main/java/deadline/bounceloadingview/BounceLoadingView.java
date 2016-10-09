9b2d75e19036628ee1d7dcd8c9232b98ee29e287
2682667136@qq.com <2682667136@qq.com>
2016-10-09 17:08:24 +0800

    no message

--- app/src/main/java/deadline/bounceloadingview/BounceLoadingView.java
+++ app/src/main/java/deadline/bounceloadingview/BounceLoadingView.java
@@ -1,16 +1,22 @@
 package deadline.bounceloadingview;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import java.util.ArrayList;
 
@@ -22,10 +28,11 @@ public class BounceLoadingView extends View {
 
     private static final String TAG = BounceLoadingView.class.getSimpleName();
 
-    private static final int SHADOW_COLOR = Color.GRAY;
+    private static final int SHADOW_COLOR = Color.LTGRAY;
 
     private int mShadowColor = SHADOW_COLOR;
 
+    //画笔
     private Paint mBitmapPaint;
     private Paint mShadowPaint;
 
@@ -33,13 +40,17 @@ public class BounceLoadingView extends View {
     //建议图片大小相同
     private ArrayList<Bitmap> mBitmapList;
     private Bitmap mCurrentBitmap;
-
-    private RectF mShadowRectF;
+    private int mCurrentIndex;
 
     private float mPercent;
 
     private float mHalfShadowHeight;
     private float mHalfShadowWidth;
+    private float mTopMargin;
+
+    private Matrix matrix;
+    private RectF mShadowRectF;
+    private ValueAnimator animator;
 
     public BounceLoadingView(Context context) {
         this(context, null);
@@ -61,43 +72,142 @@ public class BounceLoadingView extends View {
 
     private void setup() {
 
-        mPercent = 1f;
+        mPercent = 0f;
+        mCurrentIndex = 0;
+        matrix = new Matrix();
         mShadowRectF = new RectF();
         mBitmapList = new ArrayList<>();
 
         mShadowPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mShadowPaint.setStyle(Paint.Style.FILL);
         mShadowPaint.setColor(mShadowColor);
+
+        mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
 
-    public void addBitmap(@NonNull Bitmap bitmap){
-        mBitmapList.add(bitmap);
+    public void addBitmap(int bitMapId){
+        try {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), bitMapId);
+            addBitmap(bitmap);
+        }catch (Resources.NotFoundException e){
+            e.printStackTrace();
+        }
     }
-    public void setBitmaps(ArrayList<Bitmap> bitmaps){
+
+    public void addBitmap(Bitmap bitmap){
+        if(bitmap != null) {
+            mBitmapList.add(bitmap);
+        }
+    }
+
+    public void addBitmaps(ArrayList<Bitmap> bitmaps){
        if(bitmaps != null){
            mBitmapList.addAll(bitmaps);
        }
     }
 
-    public void start(){
+    public void start()
+    {
 
+        stop();
+
+        if(animator == null)
+        {
+            animator = ValueAnimator.ofFloat(0f, 1f, 0f);
+            animator.setInterpolator(new AccelerateDecelerateInterpolator());
+            animator.setRepeatCount(ValueAnimator.INFINITE);
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    float value = (float) valueAnimator.getAnimatedValue();
+
+                    if(value != mPercent){
+                        mPercent = value;
+                        postInvalidate();
+                    }
+                }
+            });
+
+            animator.addListener(new AnimatorListenerAdapter() {
+
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    //重置index
+                    mCurrentIndex = 0;
+                    mCurrentBitmap = mBitmapList.get(mCurrentIndex);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    if(mBitmapList != null && mBitmapList.size() > 0){
+                        mCurrentIndex ++;
+                        if(mCurrentIndex >= mBitmapList.size()) {
+                            mCurrentIndex = 0;
+                        }
+                        mCurrentBitmap = mBitmapList.get(mCurrentIndex);
+                    }
+                }
+            });
+        }
+
+
+        animator.setDuration(800);
+        animator.start();
+    }
+
+    public void stop(){
+        if(animator != null){
+            animator.cancel();
+            animator = null;
+        }
+    }
+
+    /**
+     * 设置阴影的颜色
+     * @param shadowColor
+     */
+    public void setShadowColor(int shadowColor){
+        this.mShadowColor = shadowColor;
+        if(mShadowPaint != null){
+            mShadowPaint.setColor(mShadowColor);
+            postInvalidate();
+        }
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width      = MeasureSpec.getSize(widthMeasureSpec);
+        int height     = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(width, 2 * width);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        mShadowRectF.set(getWidth() / 2 * mPercent,
-                0.9f * getHeight() - 0.1f * mPercent * getHeight(),
-                getWidth() * (1+ mPercent) / 2,
-                0.9f * getHeight() + 0.1f * mPercent * getHeight());
+        mHalfShadowWidth = Math.max(16, getWidth() / 2f * mPercent);
+        mHalfShadowHeight = Math.max(8, getHeight() / 8f * mPercent);
+
+        mShadowRectF.set(getWidth() / 2f - mHalfShadowWidth,
+                getHeight() * 7 / 8f - mHalfShadowHeight,
+                getWidth() / 2f + mHalfShadowWidth,
+                getHeight() * 7 / 8f + mHalfShadowHeight);
         canvas.drawOval(mShadowRectF, mShadowPaint);
+
+        if(mCurrentBitmap != null) {
+            canvas.save();
+            mTopMargin = (getHeight() * 0.9f - mCurrentBitmap.getHeight()) * mPercent;
+            canvas.translate(getWidth() / 2f - mCurrentBitmap.getWidth() / 2f, mTopMargin);
+            canvas.drawBitmap(mCurrentBitmap, matrix, mBitmapPaint);
+            canvas.restore();
+        }
+    }
+
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        stop();
     }
 }
--- app/src/main/java/deadline/bounceloadingview/MainActivity.java
+++ app/src/main/java/deadline/bounceloadingview/MainActivity.java
@@ -1,20 +1,28 @@
 package deadline.bounceloadingview;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BounceLoadingView loadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        loadingView = (BounceLoadingView) findViewById(R.id.loadingView);
+
+        loadingView.addBitmap(R.mipmap.v4);
+        loadingView.addBitmap(R.mipmap.v5);
+        loadingView.addBitmap(R.mipmap.v6);
+        loadingView.addBitmap(R.mipmap.v7);
+        loadingView.addBitmap(R.mipmap.v8);
+        loadingView.addBitmap(R.mipmap.v9);
+        loadingView.setShadowColor(Color.LTGRAY);
+        loadingView.start();
     }
 }
